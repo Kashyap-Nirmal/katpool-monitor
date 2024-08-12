fb365e160aa547aa9dcba5ea4097e2120f9a621a
argonmining <ashton@onargon.com>
2024-08-12 12:32:29 -0500

    updated database index.ts from Client to Pooled Connections to avoid oversaturating the db with connections

--- src/db.ts
+++ src/db.ts
@@ -1,51 +1,65 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import { Decimal } from 'decimal.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 console.log(`DB: Connecting DB`);
-client.connect();
 
 export async function getBalances() {
+  const client = await pool.connect();
   console.log(`DB: getting balances`);
-  const res = await client.query('SELECT miner_id, wallet, balance FROM miners_balance');
-  const balances: Record<string, Record<string, Decimal>> = {};
+  try {
+    const res = await client.query('SELECT miner_id, wallet, balance FROM miners_balance');
+    const balances: Record<string, Record<string, Decimal>> = {};
 
-  res.rows.forEach(row => {
-    const wallet = row.wallet;
-    const miner_id = row.miner_id;
-    const balance = new Decimal(row.balance);
-    if (!balances[wallet]) {
-      balances[wallet] = {};
-    }
-    balances[wallet][miner_id] = balance;
-  });
+    res.rows.forEach(row => {
+      const wallet = row.wallet;
+      const miner_id = row.miner_id;
+      const balance = new Decimal(row.balance);
+      if (!balances[wallet]) {
+        balances[wallet] = {};
+      }
+      balances[wallet][miner_id] = balance;
+    });
 
-  return balances;
+    return balances;
+  } finally {
+    client.release();
+  }
 }
 
 export async function getTotals() {
+  const client = await pool.connect();
   console.log(`DB: getting totals`);
-  const res = await client.query('SELECT address, total FROM wallet_total');
-  const totals: Record<string, Decimal> = {};
+  try {
+    const res = await client.query('SELECT address, total FROM wallet_total');
+    const totals: Record<string, Decimal> = {};
 
-  res.rows.forEach(row => {
-    const address = row.address;
-    const total = new Decimal(row.total);
-    totals[address] = total;
-  });
+    res.rows.forEach(row => {
+      const address = row.address;
+      const total = new Decimal(row.total);
+      totals[address] = total;
+    });
 
-  return totals;
+    return totals;
+  } finally {
+    client.release();
+  }
 }
 
 // New function to retrieve payments by wallet_address
 export async function getPaymentsByWallet(walletAddress: string) {
+  const client = await pool.connect();
   console.log(`DB: getting payments for wallet_address: ${walletAddress}`);
-  const res = await client.query('SELECT * FROM payments WHERE wallet_address = $1 ORDER BY timestamp DESC', [walletAddress]);
-  return res.rows;
+  try {
+    const res = await client.query('SELECT * FROM payments WHERE wallet_address = $1 ORDER BY timestamp DESC', [walletAddress]);
+    return res.rows;
+  } finally {
+    client.release();
+  }
 }
