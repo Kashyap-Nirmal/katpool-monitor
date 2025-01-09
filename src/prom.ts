295103e5fccc1360659d45b6848519befbb6c6c0
kr-kashyap <kashyap.n@knackroot.com>
2025-01-09 19:23:38 +0530

    Add getBlocks function.

--- src/prom.ts
+++ src/prom.ts
@@ -5,41 +5,6 @@ import { stringifyHashrate } from './utils';
 dotenv.config();
 const PROMETHEUS_URL = process.env.MONITORING || "http://kas.katpool.xyz:8080";
 
-async function queryPrometheus(timeInMin: number) {
-  try {
-	// Prometheus API endpoint
-	const url = `${PROMETHEUS_URL}/api/v1/query`;
-
-	console.log("Querying Prometheus...");
-    
-	// Send GET request to Prometheus API
-	const QUERY = `avg_over_time(miner_hash_rate_GHps[${timeInMin/60}h])`; // Replace with your metric name
-	console.log(QUERY)
-	const response = await axios.get(url, {
-  	params: { query: QUERY },
-	});
-
-	// Extract the data from the response
-	const data = response.data;
-
-	// Extract and log only the result if needed
-	const results = data.data?.result;
-	if (results && results.length > 0) {
-  	console.log("\nFormatted Results:");
-  	results.forEach((result: any) => {
-    	console.log({
-      	timestamp: result.value[0],
-      	hash_rate: stringifyHashrate(result.value[1]),
-    	});
-  	});
-	} else {
-  	console.log("No results found for the query.");
-	}
-  } catch (error: any) {
-	console.error("Error querying Prometheus:", error.message);
-  }
-}
-
 export async function getCurrentPoolHashRate() {
 	try {
 		const url = `${PROMETHEUS_URL}/api/v1/query`;
@@ -56,9 +21,41 @@ export async function getCurrentPoolHashRate() {
 			});
 			return hashRate
 		} else {
-		console.log("No results found for the query.");
+		console.log(`No results found for the query - ${query}.`);
 		}
 	} catch (err) {
 		console.error('Error querying pool hash rate:', err)
 	}
+}
+
+interface block_detail {
+	block_hash: string, 
+	[key : string]: string 
+}
+
+export async function getBlocks() {
+	try {
+		const url = `${PROMETHEUS_URL}/api/v1/query`;
+		const query = `paid_blocks_1min_count`
+		const response = await axios.get(url, {
+			params: { query },
+		});
+		const data = response.data;
+		const results = data.data?.result;
+		let block_details : block_detail[] = [];
+		if (results && results.length > 0) {
+			results.forEach((result: any) => {
+				let detail : block_detail = {
+					block_hash: result?.metric?.block_hash,
+					[result?.metric?.daa_score.toString()]: result?.metric?.timestamp,
+				}
+				block_details.push(detail)
+			});
+			return block_details
+		} else {
+			console.log(`No results found for the query - ${query}.`);
+		}
+	} catch (err) {
+		console.error('Error querying blocks:', err)
+	}	
 }
